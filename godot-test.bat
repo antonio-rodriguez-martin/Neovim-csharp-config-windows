746fee37df8e49bbe64ec1b7cd3d9f66247f355a
Antonio <antoniorodriguezmartin88@gmail.com>
2026-07-25 01:59:00 +0200

    Delete godot-test.bat

--- godot-test.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-echo arg1=[%1] > "%USERPROFILE%\Desktop\godot_args.txt"
-echo arg2=[%2] >> "%USERPROFILE%\Desktop\godot_args.txt"
-echo all=[%*] >> "%USERPROFILE%\Desktop\godot_args.txt"
-
-pause
